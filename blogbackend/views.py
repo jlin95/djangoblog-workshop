f6dccbedf95824c11e2ae347d401665849682f0a
Jia Lin <leeway95@gmail.com>
2023-10-07 16:44:06 +0200

    (feat): Implement CRUD for django admin form

--- blogbackend/views.py
+++ blogbackend/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import PostForm
 from .models import Post
 from django.utils import timezone
 
@@ -14,3 +15,50 @@ def post_detail(request, pk):
     return render(request, 'blogbackend/post_detail.html', {
         'post': post
     })
+
+def post_new(request):
+    if (request.method == 'POST'):
+        form = PostForm(request.POST)
+        if (form.is_valid()):
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blogbackend/post_edit.html', { 
+        'form': form 
+    })
+
+def post_edit(request, pk):
+    # different from post_new
+    post = get_object_or_404(Post, pk=pk)
+    if (request.method == 'POST'):
+        # this is different from post_new
+        form = PostForm(request.POST, instance=post)
+        if (form.is_valid()):
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            return redirect('post_detail', pk=post.pk)
+    else: 
+        form = PostForm(instance=post)
+    return render(request, 'blogbackend/post_edit.html', {
+        'form': form
+    })
+    
+
+# def post_edit(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     if request.method == "POST":
+#         form = PostForm(request.POST, instance=post)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.author = request.user
+#             post.published_date = timezone.now()
+#             post.save()
+#             return redirect('post_detail', pk=post.pk)
+#     else:
+#         form = PostForm(instance=post)
+#     return render(request, 'blog/post_edit.html', {'form': form})
