4735a25a9e0af90c7f5449f864c3f55cd8a91871
Jia Lin <leeway95@gmail.com>
2023-10-07 11:12:43 +0200

    (feat): Added url to point to home page

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,5 @@
 """blog URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Usage: 
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +12,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blogbackend.urls'))
 ]
